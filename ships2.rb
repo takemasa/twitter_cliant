9a0e91bcd247a41b54a4c7da9c8ff9bdedc1d847
Hiroyuki MORITA <morita@val.co.jp>
2013-07-26 17:10:33 +0900

    format
    インデント修正と空行の削除

--- ships2.rb
+++ ships2.rb
@@ -5,9 +5,6 @@ require 'yaml'
 
 config = YAML.load_file('./config.yaml')
 
-
-
-
 Twitter.configure do |cnf|
   cnf.consumer_key = config['consumer_key']
   cnf.consumer_secret = config['consumer_secret']
@@ -35,40 +32,40 @@ counter = 0
 
 # 無限ループ
 while counter == 0  do
-    begin
-      # 引数で受け取ったワードを元に、検索結果を取得し、古いものから順に並び替え
-      # ※最初はsince_id=0であるため、tweet ID 0以降のTweetから最新のもの上位100件を取得
-      #一度に取得できる限界数は100
-      Twitter.search(ARGV[0], :count => 100000000, :result_type => "recent", :since_id => since_id).results.reverse.map do |status|
+  begin
+    # 引数で受け取ったワードを元に、検索結果を取得し、古いものから順に並び替え
+    # ※最初はsince_id=0であるため、tweet ID 0以降のTweetから最新のもの上位100件を取得
+    #一度に取得できる限界数は100
+    Twitter.search(ARGV[0], :count => 100000000, :result_type => "recent", :since_id => since_id).results.reverse.map do |status|
 
-        # Tweet ID, ユーザ名、Tweet本文、投稿日を1件づつ表示
-        "#{status.id} :#{status.from_user}: #{status.text} : #{status.created_at} "
+      # Tweet ID, ユーザ名、Tweet本文、投稿日を1件づつ表示
+      "#{status.id} :#{status.from_user}: #{status.text} : #{status.created_at} "
 
-        p status.id
-        p "@" + status.from_user
-        p status.text
-        p status.created_at
+      p status.id
+      p "@" + status.from_user
+      p status.text
+      p status.created_at
 
-        print("\n")
-		text = status.text.gsub(/\r\n|\r|\n/," ")
-		#text = text.gsub(/[\ud000-\udfff]/," ")
-File.open("#{ARGV[0]}.txt",'a'){|f|
- f.write "#{text},#{status.created_at}\n" #@#{status.from_user}
-}
-        # 取得したTweet idをsince_idに格納
-        # ※古いものから新しい順(Tweet IDの昇順)に表示されるため、
-        #  最終的に、取得した結果の内の最新のTweet IDが格納され、
-        #  次はこのID以降のTweetが取得される
-        since_id=status.id
-      end
+      print("\n")
+      text = status.text.gsub(/\r\n|\r|\n/," ")
+      #text = text.gsub(/[\ud000-\udfff]/," ")
+      File.open("#{ARGV[0]}.txt",'a'){|f|
+        f.write "#{text},#{status.created_at}\n" #@#{status.from_user}
+      }
+      # 取得したTweet idをsince_idに格納
+      # ※古いものから新しい順(Tweet IDの昇順)に表示されるため、
+      #  最終的に、取得した結果の内の最新のTweet IDが格納され、
+      #  次はこのID以降のTweetが取得される
+      since_id=status.id
+    end
 
     # 5秒に一回が限界速度
     sleep(10)
 
     # 検索ワードで Tweet を取得できなかった場合の例外処理
-    rescue Twitter::Error::ClientError
-      # 60秒待機し、リトライ
-      sleep(5)
-      retry
+  rescue Twitter::Error::ClientError
+    # 60秒待機し、リトライ
+    sleep(5)
+    retry
   end
 end
