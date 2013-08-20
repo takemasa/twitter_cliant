54357a5635e508a55ed069861b4c5faee3683808
takemasa <takemasa@val.co.jp>
2013-08-20 19:11:41 +0900

    create autorun
    cron用のツイート自動取得プログラムを作成

--- output_tweets_auto.rb
+++ output_tweets_auto.rb
@@ -15,68 +15,118 @@ Twitter.configure do |cnf|
   cnf.oauth_token_secret = config["oauth_token_secret#{ARGV[1].to_i}"]
 end
 
-since_id = 0
-first_tw_id = 0
-last_tw_id = 0
-first_date = 0
-last_date = 0
-tw_num = 0
-limit = 0
-k = 10
+since_id = 0    # 前回実行時に最後に取得したtweetのid
+first_tw_id = 0  #
+last_tw_id = 0  #
+first_date = 0  #
+last_date = 0  #
+tw_sum = 0  #
+limit = 0  #
+arr_main = []  #
+arr_check =[]  #
+arr_error = []  #
+main_num = 0  #
+error_num = 0  #
+num = 0  #
+
   # 初回実行時はディレクトリを作成 since_idは前回取得した中で最も新しいtweetのid 前回実行時の最新tweet_idを取得、なければid = 0
+FileUtils::mkdir_p("../tweets/output/#{ARGV[0]}/log") unless FileTest.exist?("../tweets/output/#{ARGV[0]}/log")
 FileUtils::mkdir_p("../tweets/output/#{ARGV[0]}/check/") unless FileTest.exist?("../tweets/output/#{ARGV[0]}/check/")
 
-File.open("../tweets/output/#{ARGV[0]}/check/#{ARGV[0]}_#{day.year}-#{day.month}-#{day.day}-#{day.hour}_id.txt",'a+') {|f|
+File.open("../tweets/output/#{ARGV[0]}/check/#{ARGV[0]}_id.txt",'a+') {|f|
   since_id = f.readlines[-1]
   since_id = since_id.to_i
 }
 
-#ARGV[1]に検索語句 引数で受け取ったワードを元に、検索結果を取得し、古いものから順に並び替え since_id以降のtweetから時系列順に100件を取得
-until k <= 5 || limit == -1 do
+sleep(ARGV[1].to_i * 2)
+  # ARGV[0]に検索語句 引数で受け取ったワードを元に、検索結果を取得し、古いものから順に並び替え since_id以降のtweetから時系列順に100件を取得
+until limit == 10 do
   until_num = 0
   begin
     Twitter.search(ARGV[0], :count => 100, :result_type => "recent", :since_id => since_id, :lang=>"ja").results.reverse.each do |status|
       text = status.text.gsub(/(\r\n|\r|\n)/," ")
       text = text.gsub(","," ")
-      File.open("../tweets/output/#{ARGV[0]}/#{ARGV[0]}_#{day.year}-#{day.month}-#{day.day}-#{day.hour}.csv",'a'){|f|
-        f.write "#{status.created_at},#{text},#{status.user.screen_name},#{status.user.id},#{status.id},#{status.place},\n" 
-      }
 
-      if tw_num == 0
+      if status.place  # 出力は左から生成日、本文、ツイートid、ユーザ名、フォロー数、フォロワー数、ユーザid、現在地
+        arr_main[main_num] =  "#{status.created_at},#{text},#{status.id},#{status.user.screen_name},#{status.user.friends_count},#{status.user.followers_count},#{status.retweet_count},#{status.user.id},#{status.place.full_name}\n"
+      else
+        arr_main[main_num] =  "#{status.created_at},#{text},#{status.id},#{status.user.screen_name},#{status.user.friends_count},#{status.user.followers_count},#{status.retweet_count},#{status.user.id}\n"
+      end
+
+      if until_num == 0
         first_date = status.created_at
         first_tw_id = status.id
+        last_date = status.created_at
+        last_tw_id = status.id
       else
         last_date = status.created_at
         last_tw_id = status.id
       end
 
-      tw_num += 1
+      tw_sum += 1
       until_num += 1
+      main_num += 1
     end
-
-    limit += 1 
     since_id = last_tw_id
-    k = until_num
-    sleep(5)
-
+    sleep(2)
   rescue Twitter::Error::ClientError => e
-    File.open("../tweets/output/#{ARGV[0]}/check/#{ARGV[0]}_#{day.year}-#{day.month}-#{day.day}-#{day.hour}_error.txt",'a'){|f|
-      f.write "\n実行日時 #{day}\nerror : #{e}\n" 
-    }
+    arr_error[error_num] = "\n実行日時 #{day}\nerror : #{e}\n"
+    error_num += 1 
     retry
   end
 
-  if limit ==  1 && until_num != 1
-    File.open("../tweets/output/#{ARGV[0]}/check/#{ARGV[0]}_#{day.year}-#{day.month}-#{day.day}-#{day.hour}_id.txt",'a'){|f|
-      f.write "\n\n実行日時 #{day}\nFirst Tweet: #{first_date}\n#{first_tw_id}\nget_sum: #{until_num}\ntotal_sum #{tw_num}\nLatest Tweet: #{last_date}\n#{last_tw_id}" 
+  if until_num > 90
+    until_num = "************************************************************#{until_num}********************************************************************burst!?"
+  elsif until_num > 50
+    until_num = "************************************************************#{until_num}*************************burst?"
+  end
+
+  if limit ==  0 && until_num != 0
+    arr_check[limit] = "\n\n***************************************\n実行日時 #{day}\nFirst Tweet: #{first_date}\n#{first_tw_id}\ntotal_sum #{tw_sum}\nget_sum: #{until_num}\nLatest Tweet: #{last_date}\n#{last_tw_id}" 
+  elsif limit != 0 && until_num >= 1
+    arr_check[limit] =  "\n\nFirst Tweet: #{first_date}　id: #{first_tw_id}\ntotal_sum: #{tw_sum}  get_sum: #{until_num}\nLatest Tweet: #{last_date}\n#{last_tw_id}" 
+  else
+    break
+  end
+  limit += 1 
+end
+
+if day.month < 10
+  month = "0#{day.month}"
+else
+  month = day.month
+end
+if day.day < 10
+  date = "0#{day.day}"
+else
+  date = day.day
+end
+if day.hour < 10
+  hour = "0#{day.hour}"
+else
+  hour = day.hour
+end
+
+
+until num >= main_num && num >= limit && num>= error_num
+  if num <= main_num && day.min < 30
+    File.open("../tweets/output/#{ARGV[0]}/#{ARGV[0]}_#{day.year}-#{month}-#{date}-#{hour}-0.csv",'a'){|main|
+      main.write arr_main[num]
     }
-  elsif until_num >= 2
-    File.open("../tweets/output/#{ARGV[0]}/check/#{ARGV[0]}_#{day.year}-#{day.month}-#{day.day}-#{day.hour}_id.txt",'a'){|f|
-      f.write "\n\nFirst Tweet: #{first_date}\n#{first_tw_id}\nget_sum: #{until_num}\ntotal_sum #{tw_num}\nLatest Tweet: #{last_date}\n#{last_tw_id}" 
-    }
-  elsif until_num <= 1
-    File.open("../tweets/output/#{ARGV[0]}/check/#{ARGV[0]}_#{day.year}-#{day.month}-#{day.day}-#{day.hour}_id.txt",'a'){|f|
-     f.write "\n\nFirst Tweet: #{first_date}\n#{first_tw_id}\nget_sum: #{until_num}\ntotal_sum #{tw_num}\nLatest Tweet: #{first_date}\n#{first_tw_id}" 
+  elsif num <= main_num && day.min >= 30
+    File.open("../tweets/output/#{ARGV[0]}/#{ARGV[0]}_#{day.year}-#{month}-#{date}-#{hour}-30.csv",'a'){|main|
+      main.write arr_main[num]
     }
   end
+  if num <= limit
+    File.open("../tweets/output/#{ARGV[0]}/check/#{ARGV[0]}_id.txt",'a'){|check|
+      check.write arr_check[num]
+    }
+  end
+  if num <= error_num && error_num != 0
+    File.open("../tweets/output/#{ARGV[0]}/check/#{ARGV[0]}_#{day.year}-#{month}-#{date}-#{hour}_error.txt",'a'){|error|
+      error.write arr_error[num] 
+    }
+  end
+  num += 1
 end
