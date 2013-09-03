5ffce51e00e7bf12ec6f203d03ee239e505a1b28
takemasa <takemasa@val.co.jp>
2013-09-03 09:26:35 +0900

    fix ec2s3.rb
    ec2s3.rbをメソッド化

--- ec2s3.rb
+++ ec2s3.rb
@@ -5,62 +5,69 @@ Bundler.require
 require 'yaml'
 require 'fileutils'
 
-config = YAML.load_file('./config.yaml')
-day = Time.now
-
-AWS.config(
-  :access_key_id => config["aws_access_key"],
-  :secret_access_key => config["aws_secret_access_key"],
-  :s3_endpoint => config["region"]
-)
-s3 = AWS::S3.new
-
-
 # 第1引数は検索語句
-# 第2引数は格納先バケット内のディレクトリ名
-# csvファイルは現在時刻に基づいたファイル名で30分おきに作られるため、最新のファイル以外を指定してzip圧縮
-if day.month < 10
-  month = "0#{day.month}"
-else
-  month = day.month
-end
-if day.day < 10
-  date = "0#{day.day}"
-else
-  date = day.day
-end
-if day.hour < 10
-  hour = "0#{day.hour}"
-else
-  hour = day.hour
-end
-Dir.chdir("/home/ec2-user/twitter/tweets/#{ARGV[0]}/tweet")
-if day.min < 30
-        file = "#{day.year}-#{month}-#{date}-#{hour}-0.csv"
-elsif day.min >= 30
-        file = "#{day.year}-#{month}-#{date}-#{hour}-30.csv"
-end
-puts file
-files = files
-dir = Dir.glob("*.csv").each {|all_csv_file|
-	if all_csv_file != file
-		files = "#{all_csv_file}"
-    zipfile = "#{File.basename(all_csv_file)}.zip"
-    Zip::Archive.open(zipfile, Zip::CREATE) do |arc|
-    	arc.add_file(files)
+def ec2s3(keyword)
+  day = Time.now
+  config = YAML.load_file('./config.yaml')
+  # p "tweets/#{keyword}/tweet/を検索します"
+  dir_name = config[keyword]
+
+  AWS.config(
+    :access_key_id => config["aws_access_key"],
+    :secret_access_key => config["aws_secret_access_key"],
+    :s3_endpoint => config["region"]
+  )
+  s3 = AWS::S3.new
+  Dir.chdir("tweets/#{keyword}/tweet")
+  if day.month < 10
+    month = "0#{day.month}"
+  else
+    month = day.month
+  end
+  if day.day < 10
+    date = "0#{day.day}"
+  else
+    date = day.day
+  end
+  if day.hour < 10
+    hour = "0#{day.hour}"
+  else
+    hour = day.hour
+  end
+
+  if day.min < 30
+    file = "#{day.year}-#{month}-#{date}-#{hour}-0_#{dir_name}.csv"
+  elsif day.min >= 30
+    file = "#{day.year}-#{month}-#{date}-#{hour}-30_#{dir_name}.csv"
+  end
+  # puts "#{file} 以外をアップロード"
+  files = files
+  dir = Dir.glob("*.csv").each {|all_csv_file|
+    files = "#{all_csv_file}"
+    # p files
+    if File.basename(files) != file
+        zipfile = "#{File.basename(all_csv_file)}.zip"
+        Zip::Archive.open(zipfile, Zip::CREATE) do |arc|
+          arc.add_file(files)
+        end
+        # p "delete! #{files} ------------------"
+        File.delete(files)
+    elsif File.basename(files) == file
+      # p "keep! #{File.basename(files)}"
+      # p "keep! #{file}^^^^^^^^^^^^^^^^^^^"
     end
-    p "delete! #{all_csv_file} ------------------"
-    File.delete(all_csv_file)
-    elsif all_csv_file == file
-            p "keep! #{all_csv_file}"
-    p "keep! #{file}^^^^^^^^^^^^^^^^^^^"
-    end
+    dir = config["#{keyword}"]
     if zipfile
-        bucket = s3.buckets["dsb-twitter-test/tweets/#{ARGV[1]}/#{files[0..6]}"]
-        p zipfile
-        filename = zipfile
-        o = bucket.objects[filename]
-        o.write(:file => filename)
-        File.delete(zipfile)
+      bucket = s3.buckets["dsb-twitter-test/tweets/#{dir}/#{File.basename(files)[0..3]}/#{File.basename(files)[5..6]}/#{File.basename(files)[8..9]}"]
+      # p zipfile
+      # puts "dsb-twitter-test/tweets/#{config["#{keyword}"]}/#{File.basename(files)[0..3]}/#{File.basename(files)[5..6]}/#{File.basename(files)[8..9]}に格納します"
+      filename = File.basename(zipfile)
+      o = bucket.objects[filename]
+      o.write(:file => filename)
+      File.delete(zipfile)
     end
-}
+  }
+end
+
+
+ec2s3(ARGV[0])
