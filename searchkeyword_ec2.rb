f53044e6cc10218078c65046a8ee775d86d795b1
takemasa <takemasa@val.co.jp>
2013-09-06 16:17:17 +0900

    fix dir
    ファイルの出力先を変更

--- searchkeyword_ec2.rb
+++ searchkeyword_ec2.rb
@@ -9,11 +9,11 @@ def search_keyword(keyword,dir,date)
 	sum_num = 0
 	sum_ary = []
 	s = 0
-	if keyword  != nil && dir != nil && date != nil
-		puts "search #{keyword} from #{dir}/#{date}/*.csv"
+	if keyword  != nil && dir != nil
+		puts "search #{keyword} from #{dir}/#{date}*.csv"
 		Dir.chdir("work")
-		FileUtils::mkdir_p("#{dir}_include_#{keyword}") unless FileTest.exist?("#{dir}_include_#{keyword}")
-		Dir.glob("#{dir}/#{date}/*.csv").each {|all_csv_file|
+		FileUtils::mkdir_p("#{dir}_in_#{keyword}/sum") unless FileTest.exist?("#{dir}_in_#{keyword}/sum")
+		Dir.glob("#{dir}/#{date}*.csv").each {|all_csv_file|
 			i = 0
 			k = 0
 			csv_length = 0
@@ -36,7 +36,7 @@ def search_keyword(keyword,dir,date)
 			sum_num += 1
 			num = 0
 			until num >= i
-				File.open("#{dir}_include_#{keyword}/#{File.basename(all_csv_file)}",'a'){|hit_word|
+				File.open("#{dir}_in_#{keyword}/#{File.basename(all_csv_file)}",'a'){|hit_word|
 					hit_word.write hit[num]
 				}
 				num += 1
@@ -45,18 +45,18 @@ def search_keyword(keyword,dir,date)
 
 		num_num = 0
 		until num_num >= sum_num 
-		File.open("#{dir}_include_#{keyword}/sum_#{dir}_include_#{keyword}.csv",'a'){|sum|
+		File.open("#{dir}_in_#{keyword}/sum/sum_#{dir}_in_#{keyword}.csv",'a'){|sum|
 				sum.write sum_ary[num_num]
 			}
 			num_num += 1
 		end
 
-		File.open("#{dir}_include_#{keyword}/sum_#{dir}_include_#{keyword}.txt",'a'){|sum|
+		File.open("#{dir}_in_#{keyword}/sum/sum_#{dir}_in_#{keyword}.txt",'a'){|sum|
 			sum.write "キーワード#{keyword}\ntotal_sum  = #{s} / #{all_csv_length}\n"
 		}
 	else
 		puts "search keyword from dir/yyyy/mm/dd/*.csv"
-		puts "input keyword dir yyyy/mm/dd"
+		puts "input keyword dir yyyy/mm/dd/"
 	end
 end
 
